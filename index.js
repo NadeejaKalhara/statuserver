29acbe3011f858f223e29233451c8b561c2741de
Your Name <you@example.com>
2022-09-10 04:03:03 +0530

    aa

--- index.js
+++ index.js
@@ -9,32 +9,42 @@ app.use(cookieParser())
 app.use(express.static('public'))
 const path = require('path')
 var port = process.env.PORT || 3000;
-
+app.get("/_next/image", function(req, res){
+  res.redirect(301, "https://aduruthuma.ml/assets/img/LSM-Logo-Light.png")
+})
 app.get('*', function(req, res){
   console.log(req.url)
-  //request('https://aduruthuma.instatus.com'+req.url, function (error, response, body) {
- // console.error('error:', error); // Print the error if one occurred
- // res.status(200).send(body)
-try {
-  var x = request('http://aduruthuma.instatus.com'+req.url)
-  req.pipe(x)
-  
-  x.pipe(res)
-} catch (error) {
-  res.send(error)
-}
-})
+  const options = {
+    url: 'https://aduruthuma.instatus.com'+req.url,
+    headers: {
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'Connection':'keep-alive',
+      'Accept-Language':'en-US,en;q=0.9'
+    }
+  };
+  request(options, function (error, response, body) {
+ console.error('error:', error); // Print the error if one occurred
+ res.status(200).send(body)})
 
+})
+app.post('*', function(req, res){
+  console.log(req.url)
+  const options = {
+    url: 'https://aduruthuma.instatus.com'+req.url,
+    headers: {
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'Connection':'keep-alive',
+      'Accept-Language':'en-US,en;q=0.9'
+    },
+    method: 'POST',
+  };
+  request(options, function (error, response, body) {
+ console.error('error:', error); // Print the error if one occurred
+ res.status(200).send(body)})
+
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-module.exports = () => {
-  process.on('uncaughtException', (err) => {
-    console.log(err.message)
-  });
 
-  process.on('unhandledRejection', (err) => {
-console.log(err)
-  });
-};
